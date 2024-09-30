a79831816e8d92eef18b0abec63e263104615700
zodiacfireworks <martin.vuelta@gmail.com>
2024-09-30 11:06:17 -0500

    Add log processor for container apps

---
+++ src/shared/nrforwarder/processors/azure/container_apps.js
@@ -0,0 +1,71 @@
+const allowsTracing = false
+
+/**
+ * Process logs for Azure Container Apps
+ */
+function processLog (log, context, settings) {
+    let { properties, ...meta } = log;
+
+    if (properties !== undefined) {
+        if (typeof properties === 'object' && properties !== null) {
+            let structuredLog = {}
+
+            try {
+                properties.Log = JSON.parse(properties.Log);
+            } catch (error) {
+                context.warn('Can not parse properties.Log to JSON');
+            }
+
+            if (typeof properties.Log === 'object' && properties.Log !== null) {
+                let { message, ...otherProperties } = properties.Log;
+                delete properties.Log;
+
+                let newrelicDecorationProperties = Object.fromEntries(
+                    Object.entries(otherProperties).filter(
+                        (item) => settings.decorationProperties.includes(item[0])
+                    )
+                );
+
+                otherProperties = Object.fromEntries(
+                    Object.entries(otherProperties).filter(
+                        (item) => !settings.decorationProperties.includes(item[0])
+                    )
+                );
+
+                structuredLog = {
+                    ...structuredLog,
+                    ...newrelicDecorationProperties,
+                };
+
+                properties = {
+                    ...properties,
+                    ...otherProperties,
+                    message,
+                };
+            }
+
+            structuredLog = {
+                ...structuredLog,
+                [`${settings.customPropertiesPrefix}`]: properties,
+                [`${settings.customPropertiesPrefix}.meta`]: meta,
+            };
+
+            if (meta.time !== undefined) {
+                structuredLog.timestamp = new Date(meta.time).getTime();
+            }
+
+            if (properties.appName !== undefined) {
+                structuredLog.serviceName = properties.appName;
+            }
+
+            return structuredLog;
+        }
+    }
+
+    return log
+}
+
+module.exports = {
+    allowsTracing,
+    processLog,
+};
--- src/shared/nrforwarder/processors/index.js
+++ src/shared/nrforwarder/processors/index.js
@@ -1,9 +1,11 @@
 const azAPIManagementService = require('./azure/api_management_service')
 const azFunctionApp = require('./azure/function_app')
 const azDataFactory = require('./azure/data_factory')
+const azContainerApps = require('./azure/container_apps')
 
 module.exports = {
     '@azure/APIManagementService': azAPIManagementService,
     '@azure/FunctionApp': azFunctionApp,
-    '@azure/DataFactory': azDataFactory
+    '@azure/DataFactory': azDataFactory,
+    '@azure/ContainerApps': azContainerApps,
 }
