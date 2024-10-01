8dfcc1bda9ab24fbaf824aded33a3c464c912c50
zodiacfireworks <martin.vuelta@gmail.com>
2024-10-01 15:32:05 -0500

    Add data_factroy capabilities for distributed tracing visualization for small flows

--- src/shared/nrforwarder/processors/azure/container_apps.js
+++ src/shared/nrforwarder/processors/azure/container_apps.js
@@ -3,77 +3,62 @@ const allowsTracing = false
 /**
  * Process logs for Azure Container Apps
  */
-function processLog(log, context, settings) {
-    let { properties, ...meta } = log
+function processLog (log, context, settings) {
+    let { properties, ...meta } = log;
 
     if (properties !== undefined) {
-        if (typeof properties === 'string') {
-            try {
-                properties = JSON.parse(properties.replace(/'/g, '"'))
-            } catch (error) {
-                context.warn('Can not parse properties to JSON')
-            }
-        }
-
         if (typeof properties === 'object' && properties !== null) {
             let structuredLog = {}
 
             try {
-                properties.Log = JSON.parse(properties.Log)
+                properties.Log = JSON.parse(properties.Log);
             } catch (error) {
-                context.warn('Can not parse properties.Log to JSON')
+                context.warn('Can not parse properties.Log to JSON');
             }
 
             if (typeof properties.Log === 'object' && properties.Log !== null) {
-                let { message, ...otherProperties } = properties.Log
-                delete properties.Log
+                let { message, ...otherProperties } = properties.Log;
+                delete properties.Log;
 
                 let newrelicDecorationProperties = Object.fromEntries(
-                    Object.entries(otherProperties).filter((item) => settings.decorationProperties.includes(item[0]))
-                )
+                    Object.entries(otherProperties).filter(
+                        (item) => settings.decorationProperties.includes(item[0])
+                    )
+                );
 
                 otherProperties = Object.fromEntries(
-                    Object.entries(otherProperties).filter((item) => !settings.decorationProperties.includes(item[0]))
-                )
+                    Object.entries(otherProperties).filter(
+                        (item) => !settings.decorationProperties.includes(item[0])
+                    )
+                );
 
                 structuredLog = {
                     ...structuredLog,
-                    ...newrelicDecorationProperties
-                }
+                    ...newrelicDecorationProperties,
+                };
 
                 properties = {
                     ...properties,
                     ...otherProperties,
-                    message
-                }
-            } else if (typeof properties.message === 'string' && properties.message !== '') {
-                let { message, ...otherProperties } = properties
-
-                structuredLog = {
-                    ...structuredLog,
-                    message
-                }
-
-                properties = {
-                    ...otherProperties
-                }
+                    message,
+                };
             }
 
             structuredLog = {
                 ...structuredLog,
                 [`${settings.customPropertiesPrefix}`]: properties,
-                [`${settings.customPropertiesPrefix}.meta`]: meta
-            }
+                [`${settings.customPropertiesPrefix}.meta`]: meta,
+            };
 
             if (meta.time !== undefined) {
-                structuredLog.timestamp = new Date(meta.time).getTime()
+                structuredLog.timestamp = new Date(meta.time).getTime();
             }
 
             if (properties.appName !== undefined) {
-                structuredLog.serviceName = properties.appName
+                structuredLog.serviceName = properties.appName;
             }
 
-            return structuredLog
+            return structuredLog;
         }
     }
 
--- src/shared/nrforwarder/processors/azure/data_factory.js
+++ src/shared/nrforwarder/processors/azure/data_factory.js
@@ -1,4 +1,17 @@
-const allowsTracing = false
+const allowsTracing = true
+
+const Status = {
+    QUEUED: 'Queued',
+    IN_PROGRESS: 'InProgress',
+    FAILED: 'Failed',
+    SUCCEEDED: 'Succeeded',
+    CANCELLED: 'Cancelled'
+}
+
+const Category = {
+    PIPELINE_RUNS: 'PipelineRuns',
+    ACTIVITY_RUNS: 'ActivityRuns'
+}
 
 /**
  * Get log level from levelId reported by Azure Data Factory logger.
@@ -25,12 +38,21 @@ const getLogLevel = (level) => {
 /**
  * Process logs for Azure Data Factory
  */
-function logProcessor(log, context, settings) {
-    const { properties, ...meta } = log
+function logProcessor (log, context, settings) {
+    let { properties, ...meta } = log
 
     if (properties !== undefined) {
         if (typeof properties === 'object' && properties !== null) {
             let structuredLog = {}
+
+            if (typeof meta.end !== 'undefined' && typeof meta.start !== 'undefined') {
+                const endTime = new Date(meta.end).getTime()
+                const startTime = new Date(meta.start).getTime()
+
+                if (endTime > startTime) {
+                    meta.duration = endTime - startTime
+                }
+            }
 
             structuredLog = {
                 ...structuredLog,
@@ -50,6 +72,19 @@ function logProcessor(log, context, settings) {
                 structuredLog.level = getLogLevel(meta.level)
             }
 
+            if (meta.category !== undefined && (meta.category === Category.PIPELINE_RUNS || meta.category === Category.ACTIVITY_RUNS)) {
+                structuredLog['trace.id'] = meta.correlationId.replace(/-/g, '')
+
+                if (meta.category === Category.PIPELINE_RUNS) {
+                    structuredLog['span.id'] = structuredLog['trace.id'].slice(0, 16)
+                }
+
+                if (meta.category === Category.ACTIVITY_RUNS) {
+                    structuredLog['span.id'] = meta.env_dt_spanId
+                    structuredLog['parent.id'] = structuredLog['trace.id'].slice(0, 16)
+                }
+            }
+
             return structuredLog
         }
     }
@@ -57,7 +92,79 @@ function logProcessor(log, context, settings) {
     return log
 }
 
+/**
+ * Extract tracing informacion from logs for Azure Data Factory
+ */
+function tracingExtractor (buffer, context, settings) {
+    let spans = buffer
+        .map((log) => {
+            let status = log[`${settings.customPropertiesPrefix}.meta`].status
+            context.warn("*** *** ***")
+            context.warn(status)
+            context.warn(status === Status.FAILED || status === Status.SUCCEEDED || status === Status.CANCELLED)
+
+            if (status === Status.FAILED || status === Status.SUCCEEDED || status === Status.CANCELLED) {
+                context.warn("processing span ...")
+
+                let category = log[`${settings.customPropertiesPrefix}.meta`].category
+
+                let span = {
+                    'trace.id': log[`${settings.customPropertiesPrefix}.meta`].correlationId.replace(/-/g, ''),
+                    timestamp: new Date(log[`${settings.customPropertiesPrefix}.meta`].start).getTime(),
+                    attributes: {
+                        'duration.ms': log[`${settings.customPropertiesPrefix}.meta`].duration,
+                        duration: log[`${settings.customPropertiesPrefix}.meta`].duration / 1000
+                    }
+                }
+
+                if (category === Category.PIPELINE_RUNS) {
+                    span.id = span['trace.id'].slice(0, 16)
+                    span.attributes.name = 'Pipeline/' + log[`${settings.customPropertiesPrefix}.meta`].pipelineName
+                }
+
+                if (category === Category.ACTIVITY_RUNS) {
+                    span.id = log[`${settings.customPropertiesPrefix}.meta`].env_dt_spanId
+                    span.attributes['parent.id'] = span['trace.id'].slice(0, 16)
+                    span.attributes.name = 'Activity/' + log[`${settings.customPropertiesPrefix}.meta`].activityName
+                }
+
+                let error = log[`${settings.customPropertiesPrefix}`].Error
+
+                if (error !== undefined && error.message !== undefined && error.message !== "") {
+                    span.attributes['error'] = true
+                    span.attributes['error.message'] = error.message
+                    span.attributes['error.class'] = error.failureType
+                    span.attributes['error.code'] = error.errorCode
+                    if (typeof error.details === 'object') {
+                        span.attributes['error.details'] = JSON.stringify(error.details)
+                    } else {
+                        span.attributes['error.details'] = error.details
+                    }
+                }
+
+                if (log.serviceName !== undefined && log.serviceName !== null) {
+                    span.attributes['service.name'] = log.serviceName
+                }
+
+                context.warn(`Stringified Log: ${JSON.stringify([{ "span": span }])}`)
+                context.warn("*** *** ***")
+
+                return span
+            }
+            context.warn("*** *** ***")
+
+            return null
+        })
+        .filter((span) => span !== null)
+
+    if (spans.length === 0) {
+        context.warn('No spans found in logs.')
+    }
+
+    return spans
+}
 module.exports = {
     logProcessor,
+    tracingExtractor,
     allowsTracing
 }
