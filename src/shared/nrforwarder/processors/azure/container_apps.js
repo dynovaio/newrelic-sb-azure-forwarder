88e9c09877db11b54a72ee060c057bf582f6c186
zodiacfireworks <martin.vuelta@gmail.com>
2024-10-03 17:18:05 -0500

    Update log parsing for container apps

--- src/shared/nrforwarder/processors/azure/container_apps.js
+++ src/shared/nrforwarder/processors/azure/container_apps.js
@@ -3,7 +3,7 @@ const allowsTracing = false
 /**
  * Process logs for Azure Container Apps
  */
-function logProcessor (log, context, settings) {
+function logProcessor(log, context, settings) {
     console.log('log', log)
 
     let { properties, ...meta } = log
